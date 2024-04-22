26f147eb16bd89617f727ea46fe91cbcedfa5ac8
Rautelaneeraj <167713234+Rautelaneeraj@users.noreply.github.com>
2024-04-22 17:04:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,10 +14,10 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME" , "DORA_MUSICBOT")
+BOT_USERNAME = getenv("BOT_USERNAME" , "Rivtela_bot")
 
 # Don't Add style font 
-BOT_NAME = getenv("BOT_NAME" , "DORA_MUSICBOT")
+BOT_NAME = getenv("BOT_NAME" , "Rivtela_bot")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
@@ -28,7 +28,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 6896896077))
+OWNER_ID = int(getenv("OWNER_ID", 7026111964))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -38,15 +38,15 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/BRANDED-THUNDER/sexy-music",
+    "https://github.com/Rautelaneeraj/sexy-music",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FAKE_SMILE_78")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FRIENDS_CHATTING_POINT")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/teri_yaad")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FAKE_SMILE_78")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))

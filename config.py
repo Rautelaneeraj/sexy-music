64171f7a993762adca198b3ce890d0d26e6300a9
Rautelaneeraj <167713234+Rautelaneeraj@users.noreply.github.com>
2024-07-08 23:00:48 +0530

    Update config.py

--- config.py
+++ config.py
@@ -14,10 +14,10 @@ API_HASH = getenv("API_HASH")
 BOT_TOKEN = getenv("BOT_TOKEN")
 
 # Get Your bot username
-BOT_USERNAME = getenv("BOT_USERNAME" , "rautela_x_bot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "rivtela_bot")
 
 # Don't Add style font 
-BOT_NAME = getenv("BOT_NAME" , "rautela_x_bot")
+BOT_NAME = getenv("BOT_NAME" , "rivtela_bot")
 
 # Get your mongo url from cloud.mongodb.com
 MONGO_DB_URI = getenv("MONGO_DB_URI", None)
@@ -28,7 +28,7 @@ DURATION_LIMIT_MIN = int(getenv("DURATION_LIMIT", 900))
 LOGGER_ID = int(getenv("LOGGER_ID", None))
 
 # Get this value from @FallenxBot on Telegram by /id
-OWNER_ID = int(getenv("OWNER_ID", 7026111964))
+OWNER_ID = int(getenv("OWNER_ID", 7478660573))
 
 ## Fill these variables if you're deploying on heroku.
 # Your heroku app name
@@ -45,8 +45,8 @@ GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/teri_yaad")
-SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/teri_yaad")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/FRIENDS_HOME_CHANNEL")
+SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/FRIENDS_HOME_CHANNEL")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
